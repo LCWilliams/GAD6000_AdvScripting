ddc23443f0a8ad47a7be25eeae87e8d0ec0df471
Williams <lw9amt@bolton.ac.uk>
2016-12-02 16:15:00 +0000

    Interim update of engine & wheel manager scripts.  Included- updated editor settings.

--- Assets/Scripts/Vehicle/CS_VehicleEngine.cs
+++ Assets/Scripts/Vehicle/CS_VehicleEngine.cs
@@ -1,24 +1,37 @@
-﻿using UnityEngine;
+﻿/*
+AUTHOR(S): LEE WILLIAMS		DATE: 10/2016 - 01/2017
+EDITOR(S): SCOTT ANDERS
+SCRIPT HOLDERS: PF_PlayerVehicle
+INBOUND REFERENCES: CS_WheelManager | CS_PlayerDriver
+OUTBOUND REFERENCES:
+*/
+
+
+using UnityEngine;
 using System.Collections;
 
 public class CS_VehicleEngine : MonoBehaviour {
 
-    [Header("Engine")]
+    [Header("Engine: Enabled")]
     public bool v_EngineEnabled;
+    [Header("Engine: Power & Efficiency")]
+    [Range(0.01f,1.0f)]
     public float v_Efficiency; // Determines how well the engine & car components function.
     public float v_EnginePower; // The maximum amount of power the engine is capable of holding.
-    public float v_UsedPower; // How much power is currently remaining.
+    public float v_UsedPower; // How much power is currently being used.
+    public int v_MaximumTorque; // Max amount of torque the vehicle is capable of producing.
+    [Range(0,5)]
+    public int v_Gear; // Current gear of the vehicle.
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         DefaultSettings();
-        ToggleEngine();
+        ToggleEngine(); // [DEBUG] -- turns on vehicle.
 	} // END - Start
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("e"))
-        {
+        if (Input.GetKeyDown("e")){
             ToggleEngine();
         }
         AutoEngineDisable();
@@ -26,10 +39,7 @@ public class CS_VehicleEngine : MonoBehaviour {
 	} // END - Update.
 
     void DefaultSettings(){
-        v_EngineEnabled = false;
-        v_Efficiency = 1.0f;
-        v_EnginePower = 100 * v_Efficiency;
-        v_UsedPower = 0;
+        v_EnginePower = v_EnginePower * v_Efficiency;
     } // END - Default settings
 
     void AutoEngineDisable(){
--- Assets/Scripts/Vehicle/CS_WheelManager.cs
+++ Assets/Scripts/Vehicle/CS_WheelManager.cs
@@ -1,15 +1,53 @@
-﻿using UnityEngine;
+﻿/*
+AUTHOR(S): LEE WILLIAMS		DATE: 10/2016 - 01/2017
+EDITOR(S): SCOTT ANDERS
+SCRIPT HOLDERS: PF_PlayerVehicle
+INBOUND REFERENCES: CS_PlayerDriver | ((AIDriver))
+OUTBOUND REFERENCES: CS_VehicleEngine
+*/
+
+using UnityEngine;
 using System.Collections;
 
 public class CS_WheelManager : MonoBehaviour {
+    // VARIABLES
+    
+    //Scripts:
+    CS_VehicleEngine Engine; // Engine script attached to this vehicle.
 
-	// Use this for initialization
+    //Wheel Configuration Arrays.
+    [Header("Front Wheels")]
+    public GameObject[] v_WheelsFront;
+    public bool v_PowerToFront; // applies driving power to FRONT wheels.
+    public bool v_SteeringFront;
+    public bool v_BrakesFront;
+
+    [Header("Middle Wheels")]
+    public bool v_PowerToMid; // Applies driving power to MIDDLE wheels.
+    public bool v_BrakesMid;
+    public GameObject[] v_WheelsMid;
+
+    [Header("Rear Wheels")]
+    public bool v_PowerToRear; // Applies driving power to REAR wheels.
+    public bool v_SteeringRear;
+    public bool v_BrakesRear;
+    public GameObject[] v_WheelsRear;
+
+
+    // Use this for initialization
 	void Start () {
-	
+        Engine = GetComponent<CS_VehicleEngine>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+
+    void Gears() {
+        /*
+        Gear 1 : Torque : 
+        */
+    }
 }
