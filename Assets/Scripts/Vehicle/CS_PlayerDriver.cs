4171bfaec9acee83f3ca07520dba040ac746d679
Williams <lw9amt@bolton.ac.uk>
2016-12-02 16:26:33 +0000

    Update: Player driver + input manager

---
+++ Assets/Scripts/Vehicle/CS_PlayerDriver.cs
@@ -0,0 +1,25 @@
+﻿/*
+AUTHOR(S): LEE WILLIAMS		DATE: 10/2016 - 01/2017
+EDITOR(S): SCOTT ANDERS
+SCRIPT HOLDERS: PF_PlayerVehicle
+INBOUND REFERENCES: 
+OUTBOUND REFERENCES: CS_VehicleEngine | CS_WheelManager
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class CS_PlayerDriver : MonoBehaviour {
+    // VARIABLES
+    //Scripts:
+    CS_VehicleEngine Engine; // Engine script attached to this vehicle.
+
+    void Start () {
+        // Get components:
+        Engine = GetComponent<CS_VehicleEngine>();
+    } // END - Start
+	
+	void Update () {
+	    
+	}
+}
