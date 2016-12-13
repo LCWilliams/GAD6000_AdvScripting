e2d2ef4254850d0c9133cf1bda3e4ecc04f0fdeb
Anders <spa1amt@bolton.ac.uk>
2016-12-13 15:29:02 +0000

    System State Machine bullshit

---
+++ Assets/Scripts/SceneManager/GUIScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIScript : MonoBehaviour {
+    public Text currentStateText;
+    public SystemStateMachine systemSM;
+
+	// Use this for initialization
+	void Start () {
+        systemSM = SystemStateMachine.get_instance();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentStateText.text = systemSM.get_current_state();
+	}
+
+    public void game_over_lost()
+    {
+        systemSM.handle_transition("GameOverLost");
+        SceneLoader.get_instance().load_appropriate_scene();
+    }
+
+    public void game_over_won()
+    {
+        systemSM.handle_transition("GameOverWon");
+        SceneLoader.get_instance().load_appropriate_scene();
+    }
+
+    public void ok_button()
+    {
+        systemSM.handle_transition("OkButton");
+        SceneLoader.get_instance().load_appropriate_scene();
+    }
+
+    public void credits()
+    {
+        systemSM.handle_transition("CreditsPressed");
+        SceneLoader.get_instance().load_appropriate_scene();
+    }
+
+    public void replay()
+    {
+        systemSM.handle_transition("ReplyPressed");
+        SceneLoader.get_instance().load_appropriate_scene();
+    }
+}
---
+++ Assets/Scripts/SceneManager/SystemStateMachine.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SystemStateMachine : MonoBehaviour
+{
+    static SystemStateMachine instance = null;
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static SystemStateMachine get_instance()
+    {
+        return instance;
+    }
+
+    private List<string> stateList;
+    private List<string> transitionList;
+    private Dictionary<string, string> stateTransitionTable;
+    private string currentState = "";
+    void Start()
+    {
+        set_initial_state("SplashScreen");
+        set_state_transition("SplashScreen", "OkButton", "MenuScreen");
+        set_state_transition("MenuScreen", "Play", "GameScreen");
+        set_state_transition("MenuScreen", "CreditsPressed", "Credits");
+        set_state_transition("Credits", "OkButton", "MenuScreen");
+        set_state_transition("GameScreen", "GameOverLost", "YouLostScreen");
+        set_state_transition("GameScreen", "GameOverWon", "YouWonScreen");
+        set_state_transition("YouLostScreen", "ReplayPressed", "MenuScreen");
+        set_state_transition("YouWonScreen", "ReplayPressed", "MenuScreen");
+        SceneLoader.get_instance().load_appropriate_scene();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void set_initial_state(string stateName)
+    {
+        currentState = stateName;
+    }
+
+    public string get_current_state()
+    {
+        return currentState;
+    }
+
+    public void set_state_transition(string ifCurrentNameIs, string receiveTransition, string newStateIs)
+    {
+        string key;
+
+        if (!stateList.Contains(ifCurrentNameIs)) stateList.Add(ifCurrentNameIs);
+        if (!stateList.Contains(newStateIs)) stateList.Add(newStateIs);
+        if (!transitionList.Contains(receiveTransition)) transitionList.Add(receiveTransition);
+        key = ifCurrentNameIs + "==>" + receiveTransition;
+        if (!stateTransitionTable.ContainsKey(key))
+        {
+            stateTransitionTable[key] = newStateIs;
+        }
+    }
+
+    public string handle_transition(string transition)
+    {
+        string key;
+        key = currentState + "==>" + transition;
+        if (stateTransitionTable.ContainsKey(key))
+        {
+            currentState = stateTransitionTable[key];
+        }
+
+        return currentState;
+    }
+}
