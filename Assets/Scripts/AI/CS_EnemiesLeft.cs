559960d89dabf6c8cb0b0708369757217a6f8fec
ScottsGamingRig\Scotts Gaming Rig <scottymoldoon@gmail.com>
2017-01-04 20:23:00 +0000

    EnemiesLeft/LoseScene

--- Assets/Scripts/AI/CS_EnemiesLeft.cs
+++ Assets/Scripts/AI/CS_EnemiesLeft.cs
@@ -1,6 +1,15 @@
-﻿using UnityEngine;
+﻿/*
+AUTHOR(S): SCOTT ANDERS 
+EDITOR(S): LEE WILLIAMS
+SCRIPT HOLDERS: 
+INBOUND REFERENCES: 
+OUTBOUND REFERENCES: 
+OVERVIEW:  Get the tag of the AI vehicles, create an array and load the win scene if no enemies remain
+*/
+using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CS_EnemiesLeft : MonoBehaviour {
     public GameObject[] v_EnemiesLeft;
@@ -16,4 +25,9 @@ public class CS_EnemiesLeft : MonoBehaviour {
             SceneManager.LoadScene("Win");
         }
 	}
+
+    void FixedUpdate()
+    {
+        Debug.Log("Enemies Left=" + v_EnemiesLeft);
+    }
 }
