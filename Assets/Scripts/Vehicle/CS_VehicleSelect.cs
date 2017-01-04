f64a3d5d26b1547bfefb1b23611df3f5fcd4b65b
LWD00\Lee Connor Williams <LCWilliams@gmx.co.uk>
2017-01-04 23:55:33 +0000

    fak

---
+++ Assets/Scripts/Vehicle/CS_VehicleSelect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CS_VehicleSelect : MonoBehaviour {
+
+    public GameObject go_PlayerProfile;
+    CS_PlayerProfile v_PlayerProfile;
+
+
+    // Use this for initialization
+    void Start () {
+        go_PlayerProfile = GameObject.Find("PlayerProfile");
+        v_PlayerProfile = go_PlayerProfile.GetComponent<CS_PlayerProfile>();
+	}
+	
+    public void SelectBasilisk() {
+        v_PlayerProfile.v_Vehicle = 0;
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void SelectLeviathan(){
+        v_PlayerProfile.v_Vehicle = 1;
+        SceneManager.LoadSceneAsync(1);
+    }
+
+
+}
